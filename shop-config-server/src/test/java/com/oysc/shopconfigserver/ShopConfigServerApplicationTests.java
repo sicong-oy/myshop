cda3c6e4e03e9f57e0997a70fdd81fbd816c2b4b
oysc <ouyangsicong@foxmail.com>
2022-02-19 23:37:30 +0800

    第一次提交

---
+++ shop-config-server/src/test/java/com/oysc/shopconfigserver/ShopConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oysc.shopconfigserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopConfigServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
